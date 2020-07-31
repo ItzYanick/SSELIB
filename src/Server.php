42b8c9330941bc565e290d6f3283cfee1970643d
Yanick Stephan <yanick@radar-system.net>
2020-07-31 11:10:58 +0200

    idk. but i guess its gonna work

--- src/Server.php
+++ src/Server.php
@@ -1,23 +1,30 @@
 <?php
+
 namespace ItzYanick\SSELIB;
-class Server {
+
+class Server
+{
     protected $event;
     protected $prevData = '{}';
 
-    public function __construct(Event $event) {
+    public function __construct(Event $event)
+    {
         $this->event = $event;
     }
-    public function sendEvent() {
+    public function sendEvent()
+    {
         $data = $this->event->getDataContent($this->prevData);
-        
-        //TODO CHECK IF FULL
-        echo $data;
-        ob_flush();
-        flush();
-        
-        $prevData = $data;
+
+        if ($data::asArray['id'] != '') {
+            echo $data;
+            ob_flush();
+            flush();
+        }
+
+        $prevData = $data::asArray['data'];
     }
-    public function startServer($interval = 5) {
+    public function startServer($interval = 5)
+    {
         while (true) {
             $this->sendEvent();
             if (connection_aborted()) {
